87e4f721fd5a4536e95512b95384b9e4b8cfdd6f
Mariano Nicolás Molina <marianoo.molina@gmail.com>
2017-09-10 22:09:51 -0300

    Desarrollo de avioneta

--- src/ejercito/Avioneta.java
+++ src/ejercito/Avioneta.java
@@ -1,5 +1,27 @@
 package ejercito;
 
-public class Avioneta {
+public class Avioneta extends Unidad {
+	
+	private final static double DIST_MAX=1700;
+	private final static double DIST_MIN=500;
+	
+	public Avioneta() {
+		this.pesoMax=1000;
+		this.volMax=500000;
+	}
+
+	@Override
+	public boolean puedoCargar(Armamento armamento) {
+		return pesoMax>=armamento.getPeso() && 
+				volMax>=armamento.getVolumen() &&
+				armamento.getDistancia() <= DIST_MAX &&
+				armamento.getDistancia() >= DIST_MIN;
+	}
+
+	@Override
+	public void cargo(Armamento armamento) {
+		pesoMax -= armamento.getPeso();
+		volMax -= armamento.getVolumen();		
+	}
 
 }
